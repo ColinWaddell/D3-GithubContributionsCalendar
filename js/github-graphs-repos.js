542b479f95b69c9d70723262ae9460a8cbff778d
Colin Waddell <mrcolin@gmail.com>
2013-10-01 23:28:25 +0100

    basic repo listing and graphing is working. isn't verry pretty though

--- js/github-graphs-repos.js
+++ js/github-graphs-repos.js
@@ -1,63 +1,67 @@
+
 // Build some example graphs
-var graph = new Gitgraph({ 
-        user    : 'nex3',
-        repo    : 'sass',
-        domNode : document.getElementById("graphDiv"),
-        width   : 800,
-        height  : 170,
-        allColor: "rgb(202, 202, 202)",
-        userColor: "rgb(51, 102, 153)",
-        background: "white"
-});
-graph.className += "graph";
-
-var graph2 = new Gitgraph({ 
-        user    : 'josephg',
-        repo    : 'sharejs',
-        domNode : document.getElementById("graphDiv2"),
-        width   : 800,
-        height  : 50,
-        allColor: "#DE0052",
-        userColor: "#4C10AE",
-        background: "#AAA"
-});
-graph2.className += "graph";
-
-var graph3 = new Gitgraph({ 
-        user    : 'imathis',
-        repo    : 'octopress',
-        domNode : document.getElementById("graphDiv3"),
-        width   : 800,
-        height  : 100,
-        allColor: "#FFD073",
-        userColor: "#FF4040",
-        background: "#5CCCCC"
-});
-graph3.className += "graph";
-
-var graph4 = new Gitgraph({ 
-        user    : 'nex3',
-        repo    : 'sass',
-        domNode : document.getElementById("graphDiv4"),
-        width   : 800,
-        height  : 70,
-        allColor: "#025167",
-        userColor: "#86B32D",
-        background: "#FF7D73"
-});
-graph4.className += "graph";
 
 
-// Guages tracking
-var _gauges = _gauges || [];
-(function() {
-        var t   = document.createElement('script');
-        t.type  = 'text/javascript';
-        t.async = true;
-        t.id    = 'gauges-tracker';
-        t.setAttribute('data-site-id', '50b93314f5a1f55a5800000c');
-        t.src = '//secure.gaug.es/track.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(t, s);
-}());
 
+function gitFancyRepos(settings){
+  var plugin = {
+
+    settings: {
+      username: 'ColinWaddell',
+      target: '#my-github-projects'
+    },
+
+    init: function(){
+      this._loadUserData();      
+      this.target = $(this.settings.target);
+      this.target.html("<span>Querying GitHub for " + this.settings.username +"'s repositories...</span>");
+    },
+
+    _loadUserData: function(){          
+      $.getJSON('https://api.github.com/users/'+this.settings.username+'/repos?callback=?', this._handleUserData.bind(this));
+    },
+
+    _sortByName: function(repos) {
+        repos.sort(function(a,b) {
+          return a.name - b.name;
+       });
+    },
+
+    _handleUserData: function(data) {
+      var repos = data.data; // JSON Parsing
+      this._sortByName(repos);    
+   
+      this.target.empty();
+
+      $(repos).each(function(index, value) {
+        if (value.name !== (this.settings.username.toLowerCase()+'.github.com')) {
+          /*list.append('<dt><a href="'+ */
+            //(value.homepage?value.homepage:value.html_url) +'">' + 
+            //value.name + '</a> <em>'+(value.language?('('+value.language+')'):'')+'</em></dt>');
+          /*list.append('<dd>' + value.description +'</dd>');*/
+
+          var id = 'gitgraph'+index;
+          this.target.append("<div id='"+id+"'></div>");
+          var graph = new Gitgraph({ 
+            user    : this.settings.username,
+            repo    : value.name,
+            domNode : document.getElementById(id),
+            width   : 800,
+            height  : 170,
+            allColor: "rgb(202, 202, 202)",
+            userColor: "rgb(51, 102, 153)",
+            background: "white"
+          });
+
+        }
+      }.bind(this)); 
+    },
+      
+  };
+
+  $.extend(this, plugin);
+  $.extend(this.settings, settings);
+  this.init();
+}
+
+var g = new gitFancyRepos();
