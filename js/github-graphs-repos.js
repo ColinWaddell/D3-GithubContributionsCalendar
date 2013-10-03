c6c98be3ee9e28b7a667430e48163d2e422e0028
Colin Waddell <mrcolin@gmail.com>
2013-10-03 14:35:14 +0100

    sucessfully builds a stacked area graph of repo activity

--- js/github-graphs-repos.js
+++ js/github-graphs-repos.js
@@ -1,24 +1,24 @@
+function gitFancyRepos(target, settings){
+  var properties = 
+  {
+    _graphData: [],
+    _target : [],
+  };
 
-// Build some example graphs
-
-
-
-function gitFancyRepos(settings){
   var plugin = {
 
     settings: {
       username: 'ColinWaddell',
-      target: '#my-github-projects'
+      xaxis_title: '',
+      yaxis_title: 'Number of Commits'
     },
+
 
     init: function(){
-      this._loadUserData();      
-      this.target = $(this.settings.target);
-      this.target.html("<span>Querying GitHub for " + this.settings.username +"'s repositories...</span>");
-    },
-
-    _loadUserData: function(){          
-      $.getJSON('https://api.github.com/users/'+this.settings.username+'/repos?callback=?', this._handleUserData.bind(this));
+      this._target.empty();
+      this._target.addClass("git-graph");
+      this._buildGraph();
+      this._pullUserData();      
     },
 
     _sortByName: function(repos) {
@@ -27,41 +27,114 @@ function gitFancyRepos(settings){
        });
     },
 
+    _buildGraphData: function(repo){
+  
+      //var other_values = [];
+      var owner_values = [],
+          d = new Date(),
+          i = 0;
+
+      d.setDate(d.getDate() - 7*52);
+
+      for (i=0; i<52; i++){
+        //other_values.push([d.valueOf(), repo.activity.all[i]]);
+        owner_values.push([d.valueOf(), repo.activity.owner[i]]);
+        d.setDate(d.getDate() + 7);
+      }
+
+      return owner_values;
+    },
+
+    _buildGraph: function(){
+      var colors = d3.scale.category20();
+      keyColor = function(d, i) {return colors(d.key);};
+      this._graphData = [];
+
+      this._target.empty().append("<svg></svg>");
+      var id = "#" + this._target.get(0).id + " svg";
+
+      nv.addGraph(function() {
+        this._chart = nv.models.stackedAreaChart()
+                      .showControls(false)
+                      .x(function(d) { return d[0];})
+                      .y(function(d) { return d[1];})
+                      .transitionDuration(300)
+                      .color(keyColor);
+                      //.clipEdge(true);
+
+
+        if(this.settings.xaxis_title!==''){
+          this._chart.options({margin:{bottom: 100}})
+                     .showXAxis(true);
+        }
+
+        if(this.settings.yaxis_title!==''){
+          this._chart.options({margin:{left: 100}})
+                     .showYAxis(true);
+        }
+
+        this._chart.xAxis.axisLabel(this.settings.xaxis_title)
+                         .tickFormat(function(d) { return d3.time.format('%x')(new Date(d));});
+
+        this._chart.yAxis.axisLabel(this.settings.yaxis_title)
+                         .tickFormat(d3.format(',.2f'));
+
+        d3.select(id)
+          .datum(this._graphData)
+          .call(this._chart);
+
+        nv.utils.windowResize(this._chart.update);
+        return this._chart;
+      }.bind(this));
+              
+    },
+
+    _addGraphData: function(name, data){
+      if (typeof(this._graphData)==="object"){
+        this._graphData.push( { "key" : name, "values" : data} );
+
+        d3.select("#" + this._target.get(0).id + " svg")
+          .datum(this._graphData)
+          .transition()
+          .duration(500)
+          .call(this._chart);
+      }
+    },
+
+    _pullActivityData: function(repo){
+      var proxy = 'http://cdn.bitpshr.net/simple-proxy/simple-proxy.php?url=',
+          url = 'https://github.com/'+this.settings.username+'/'+repo.name+'/graphs/owner_participation';
+
+      $.getJSON(proxy+url, this._handleActivityData.bind(this, repo));
+    },
+
+    _pullUserData: function(){
+      $.getJSON('https://api.github.com/users/'+this.settings.username+'/repos?callback=?', 
+        this._handleUserData.bind(this));
+    },
+
     _handleUserData: function(data) {
       var repos = data.data; // JSON Parsing
       this._sortByName(repos);    
    
-      this.target.empty();
-
       $(repos).each(function(index, value) {
         if (value.name !== (this.settings.username.toLowerCase()+'.github.com')) {
-          /*list.append('<dt><a href="'+ */
-            //(value.homepage?value.homepage:value.html_url) +'">' + 
-            //value.name + '</a> <em>'+(value.language?('('+value.language+')'):'')+'</em></dt>');
-          /*list.append('<dd>' + value.description +'</dd>');*/
-
-          var id = 'gitgraph'+index;
-          this.target.append("<div id='"+id+"'></div>");
-          var graph = new Gitgraph({ 
-            user    : this.settings.username,
-            repo    : value.name,
-            domNode : document.getElementById(id),
-            width   : 800,
-            height  : 170,
-            allColor: "rgb(202, 202, 202)",
-            userColor: "rgb(51, 102, 153)",
-            background: "white"
-          });
-
+          this._pullActivityData(value);
         }
       }.bind(this)); 
+    },
+
+    _handleActivityData: function(repo, activity){
+      repo.activity = activity;
+      this._addGraphData(repo.name, this._buildGraphData(repo));
     },
       
   };
 
-  $.extend(this, plugin);
+  $.extend(this, plugin, properties);
   $.extend(this.settings, settings);
+  this._target = $(target);
   this.init();
 }
 
-var g = new gitFancyRepos();
+var g = new gitFancyRepos('#my-github-projects');
